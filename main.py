51b05d568d3fb0cc716f28a445a355ed3b66c8ff
mewrion <gaumengyew@gmail.com>
2022-05-03 19:35:43 +0800

    Added ability to check number of employees
    1. Can now check number of employees
    2. Updated error handling to be specific to each field
    3. Minor bugs

--- main.py
+++ main.py
@@ -1,86 +1,42 @@
-#############################
-### LinkedIn Data Scraper ###
-#############################
+from googlesearch import search   
+import csv
+import scraper as dataScraper
 
+def main(): 
+    links = []
+    file = open("linkedins.csv", encoding = "utf8")
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-import time
-import login_details
-
-
-def main(linkedins):
-
-    # Open the driver
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://linkedin.com/uas/login")
-
-    # entering username
-    username = driver.find_element_by_id("username")
-
-    # Enter Your Email Address
-    username.send_keys(login_details.email)  
-      
-    # entering password
-    pword = driver.find_element_by_id("password")
-
-    # Enter Your Password
-    pword.send_keys(login_details.password)
-
-    # Clicking on the log in button
-    # Format (syntax) of writing XPath --> 
-    # //tagname[@attribute='value']
-    driver.find_element_by_xpath("//button[@type='submit']").click()
-    time.sleep(5)
-    result = []
-    
-    for company in linkedins:
-        if company == "Blank":
-            result.append("Blank");
-            result.append("Blank");
-            continue;
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        if len(row) == 0:
+            links.append("Blank")
+        else:
+            links.append(row[0])
         
-        driver.get(company)
-        start = time.time()
-          
-        # will be used in the while loop
-        initialScroll = 0
-        finalScroll = 1000
+    
+    uncleanedData = dataScraper.main(links)
+    industry = []
+    country = []
+    employees = [] 
+    for item in range(0,len(uncleanedData),3):
+        industry.append(uncleanedData[item])
+    for item in range(1,len(uncleanedData),3):
+        country.append(uncleanedData[item])
+    for item in range(2,len(uncleanedData),3):
+        employees.append(uncleanedData[item])
 
-        while True:
-            driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
-            # this command scrolls the window starting from
-            # the pixel value stored in the initialScroll 
-            # variable to the pixel value stored at the
-            # finalScroll variable
-            initialScroll = finalScroll
-            finalScroll += 1000
-          
-            # we will stop the script for 3 seconds so that 
-            # the data can load
-            time.sleep(3)
-            # You can change it as per your needs and internet speed
-          
-            end = time.time()
-            # We will scroll for 20 seconds.
-            # You can change it as per your needs and internet speed
-            if round(end - start) > 20:
-                break
-          
-        # Scrape the lxml page for the data we need
-        src = driver.page_source
+    write_csv(industry, "industry")
+    write_csv(country, "country")
+    write_csv(employees,"employees")
+    print(uncleanedData)
+def write_csv(data, filename):
+    
+    # PreCon: Data must be a list, filename MUST be a string
+    with open(filename+".csv", "w", encoding = "utf8") as f:
+        for line in data:
+            f.write("%s\n"%line)
 
-        soup = BeautifulSoup(src, 'lxml')
-        intro = soup.find('div', {'class': "ph5 pt3"})
-        try:  
-            items = intro.find_all("div", {'class': 'org-top-card-summary-info-list__info-item'})
-            result.append(items[0].get_text().strip().replace(",", ";"))
-            result.append(items[1].get_text().strip().replace(",", ";"))
-        except:
-            result.append("Error");
-            result.append("Error");
-    driver.quit()
-    return result
+
+
 if __name__ == '__main__':
-    html_extract = main(link)
+    html_extract = main()
---
+++ scraper.py
@@ -0,0 +1,98 @@
+#############################
+### LinkedIn Data Scraper ###
+#############################
+
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+import login_details
+
+
+def main(linkedins):
+
+    # Open the driver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://linkedin.com/uas/login")
+
+    # entering username
+    username = driver.find_element_by_id("username")
+
+    # Enter Your Email Address
+    username.send_keys(login_details.email)  
+      
+    # entering password
+    pword = driver.find_element_by_id("password")
+
+    # Enter Your Password
+    pword.send_keys(login_details.password)
+
+    # Clicking on the log in button
+    # Format (syntax) of writing XPath --> 
+    # //tagname[@attribute='value']
+    driver.find_element_by_xpath("//button[@type='submit']").click()
+    time.sleep(5)
+    result = []
+    
+    for company in linkedins:
+        if company == "Blank":
+            result.append("Blank");
+            result.append("Blank");
+            result.append("Blank");
+            continue;
+        
+        driver.get(company)
+        start = time.time()
+          
+        # will be used in the while loop
+        initialScroll = 0
+        finalScroll = 1000
+
+        while True:
+            driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
+            # this command scrolls the window starting from
+            # the pixel value stored in the initialScroll 
+            # variable to the pixel value stored at the
+            # finalScroll variable
+            initialScroll = finalScroll
+            finalScroll += 1000
+          
+            # we will stop the script for 3 seconds so that 
+            # the data can load
+            time.sleep(3)
+            # You can change it as per your needs and internet speed
+          
+            end = time.time()
+            # We will scroll for 20 seconds.
+            # You can change it as per your needs and internet speed
+            if round(end - start) > 15:
+                break
+          
+        # Scrape the lxml page for the data we need
+        src = driver.page_source
+
+        soup = BeautifulSoup(src, 'lxml')
+        intro = soup.find('div', {'class': "ph5 pt3"})
+        try:  
+            items = intro.find_all("div", {'class': 'org-top-card-summary-info-list__info-item'})
+            result.append(items[0].get_text().strip().replace(",", ";"))
+        except:
+            result.append("No Industry");
+        try:
+            result.append(items[1].get_text().strip().replace(",", ";"))
+        except:
+            result.append("No Country");
+        try:
+            employees = intro.find("span", {'class': "org-top-card-secondary-content__see-all t-normal t-black--light link-without-visited-state link-without-hover-state"})
+            result.append(employees.get_text().strip().replace(",",""))
+        except:
+            result.append("No Employee Information");
+
+
+
+        
+    driver.quit()
+    return result
+if __name__ == '__main__':
+    html_extract = main(link)
